2625c38399707fe145553f84362c72efc2dd3ceb
godfreyJo <godfreyoyugi@gmail.com>
2019-02-25 23:50:45 +0300

    addition of snippets

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# Create your models here.
+
+LEXER = [item for item in get_all_lexers()if item[1]]
+LANGUAGE_CHOICE = sorted([(item [1][0], item[0])for item in LEXER])
+STYLE_CHOICES = sorted((item,item)for item in get_all_styles())
